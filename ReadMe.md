553e6ed3e9c6fd39cc655f14a343e3d9f33d8674
stricq <stricq@stricq.com>
2019-04-20 11:33:16 -0600

    Add project files.

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+# STR.MvvmCommon
+Common MVVM code for STR WPF applications
---
+++ Str.MvvmCommon/Mvvm/ObservableObject.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using Str.Common.Extensions;
+
+
+namespace Str.MvvmCommon.Mvvm {
+
+  [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+  [SuppressMessage("ReSharper", "UnusedMember.Global")]
+  public class ObservableObject :  INotifyPropertyChanged {
+
+    #region INotifyPropertyChanged Implementation
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion INotifyPropertyChanged Implementation
+
+    #region Properties
+
+    public static bool IsDesignMode {
+      get {
+        using(Process process = Process.GetCurrentProcess()) {
+          string name = process.ProcessName.ToLower().Trim();
+
+          return name == "devenv" || name == "xdesproc";
+        }
+      }
+    }
+
+    #endregion Properties
+
+    #region Protected Methods
+
+    protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpresssion) {
+      if (propertyExpresssion == null) return;
+
+      if (!(propertyExpresssion.Body is MemberExpression memberInfo)) throw new ArgumentException("The parameter is not a property.", nameof(propertyExpresssion));
+
+      PropertyInfo propertyInfo = memberInfo.Member as PropertyInfo;
+
+      if (propertyInfo == null) throw new ArgumentException("The specified property does not exist.", nameof(propertyExpresssion));
+
+      onPropertyChanged(new PropertyChangedEventArgs(propertyInfo.Name));
+    }
+
+    protected void RaisePropertyChanged() {
+      onPropertyChanged(new PropertyChangedEventArgs(String.Empty));
+    }
+
+    protected bool SetField(ref double field, double value, params Expression<Func<double>>[] selectorArray) {
+      if (value.HasMinimalDifference(field)) return false;
+
+      field = value;
+
+      if (selectorArray == null || selectorArray.Length == 0) RaisePropertyChanged();
+      else selectorArray.ForEach(RaisePropertyChanged);
+
+      return true;
+    }
+
+    protected bool SetField<T1>(ref double field, double value, Expression<Func<double>> selector, Expression<Func<T1>> selector1) {
+      if (value.HasMinimalDifference(field)) return false;
+
+      field = value;
+
+      RaisePropertyChanged(selector);
+      RaisePropertyChanged(selector1);
+
+      return true;
+    }
+
+    protected bool SetField<T1, T2>(ref double field, double value, Expression<Func<double>> selector, Expression<Func<T1>> selector1, Expression<Func<T2>> selector2) {
+      if (value.HasMinimalDifference(field)) return false;
+
+      field = value;
+
+      RaisePropertyChanged(selector);
+      RaisePropertyChanged(selector1);
+      RaisePropertyChanged(selector2);
+
+      return true;
+    }
+
+    protected bool SetField<T1, T2, T3>(ref double field, double value, Expression<Func<double>> selector, Expression<Func<T1>> selector1, Expression<Func<T2>> selector2, Expression<Func<T3>> selector3) {
+      if (value.HasMinimalDifference(field)) return false;
+
+      field = value;
+
+      RaisePropertyChanged(selector);
+      RaisePropertyChanged(selector1);
+      RaisePropertyChanged(selector2);
+      RaisePropertyChanged(selector3);
+
+      return true;
+    }
+
+    protected bool SetField<T>(ref T field, T value, params Expression<Func<T>>[] selectorArray) {
+      if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+      field = value;
+
+      if (selectorArray == null || selectorArray.Length == 0) RaisePropertyChanged();
+      else selectorArray.ForEach(RaisePropertyChanged);
+
+      return true;
+    }
+
+    protected bool SetField<T, T1>(ref T field, T value, Expression<Func<T>> selector, Expression<Func<T1>> selector1) {
+      if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+      field = value;
+
+      RaisePropertyChanged(selector);
+      RaisePropertyChanged(selector1);
+
+      return true;
+    }
+
+    protected bool SetField<T, T1, T2>(ref T field, T value, Expression<Func<T>> selector, Expression<Func<T1>> selector1, Expression<Func<T2>> selector2) {
+      if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+      field = value;
+
+      RaisePropertyChanged(selector);
+      RaisePropertyChanged(selector1);
+      RaisePropertyChanged(selector2);
+
+      return true;
+    }
+
+    protected bool SetField<T, T1, T2, T3>(ref T field, T value, Expression<Func<T>> selector, Expression<Func<T1>> selector1, Expression<Func<T2>> selector2, Expression<Func<T3>> selector3) {
+      if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+      field = value;
+
+      RaisePropertyChanged(selector);
+      RaisePropertyChanged(selector1);
+      RaisePropertyChanged(selector2);
+      RaisePropertyChanged(selector3);
+
+      return true;
+    }
+
+    protected bool SetField<T, T1, T2, T3, T4>(ref T field, T value, Expression<Func<T>> selector, Expression<Func<T1>> selector1, Expression<Func<T2>> selector2, Expression<Func<T3>> selector3, Expression<Func<T4>> selector4) {
+      if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+      field = value;
+
+      RaisePropertyChanged(selector);
+      RaisePropertyChanged(selector1);
+      RaisePropertyChanged(selector2);
+      RaisePropertyChanged(selector3);
+      RaisePropertyChanged(selector4);
+
+      return true;
+    }
+
+    protected bool SetField<T, T1, T2, T3, T4, T5>(ref T field, T value, Expression<Func<T>> selector, Expression<Func<T1>> selector1, Expression<Func<T2>> selector2, Expression<Func<T3>> selector3, Expression<Func<T4>> selector4, Expression<Func<T5>> selector5) {
+      if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+      field = value;
+
+      RaisePropertyChanged(selector);
+      RaisePropertyChanged(selector1);
+      RaisePropertyChanged(selector2);
+      RaisePropertyChanged(selector3);
+      RaisePropertyChanged(selector4);
+      RaisePropertyChanged(selector5);
+
+      return true;
+    }
+
+    #endregion Protected Methods
+
+    #region Private Methods
+
+    private void onPropertyChanged(PropertyChangedEventArgs pce) {
+      PropertyChanged?.Invoke(this, pce);
+    }
+
+    #endregion Private Methods
+
+  }
+
+}
