231919442cd60b93341a1617c0841b7ea0697217
stricq <stricq@stricq.com>
2019-04-30 13:15:24 -0600

    Final additions for MvvmCommon.

---
+++ Str.MvvmCommon/Contracts/IViewTagMetadata.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Str.MvvmCommon.Contracts {
+
+  public interface IViewTagMetadata {
+
+    string Name { get; }
+
+    string Data { get; }
+
+    string Tag  { get; }
+
+  }
+
+}
---
+++ Str.MvvmCommon/Core/ViewTagAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace Str.MvvmCommon.Core {
+
+  [MetadataAttribute]
+  [AttributeUsage(AttributeTargets.Class)]
+  [SuppressMessage("ReSharper", "UnusedMember.Global")]
+  public class ViewTagAttribute : ExportAttribute {
+
+    public string Name { get; set; }
+
+    public string Data { get; set; }
+
+    public string Tag  { get; set; }
+
+  }
+
+}
