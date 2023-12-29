d589c9b86936af2db56a94c118aa86d7e874ff61
stricq <stricq@stricq.com>
2023-12-29 00:22:29 -0700

    Add RelayCommandHelper

---
+++ Str.MvvmCommon/Helpers/RelayCommandHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+using Str.MvvmCommon.Core;
+
+
+namespace Str.MvvmCommon.Helpers;
+
+
+[UsedImplicitly]
+public static class RelayCommandHelper {
+
+  [UsedImplicitly]
+  public static RelayCommandAsync EmptyRelayCommand { get; } = new(() => Task.CompletedTask);
+
+  [UsedImplicitly]
+  public static RelayCommandAsync<T> EmptyRelayCommandT<T>() {
+    return new RelayCommandAsync<T>(_ => Task.CompletedTask);
+  }
+
+}
